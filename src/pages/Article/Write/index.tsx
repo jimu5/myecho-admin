c57b2f4817210f9c3ecd6dd72832345aecc6b884
Kimiato <853447650@qq.com>
2022-05-17 00:50:16 +0800

    feat: 发布设置盒子

--- src/pages/Article/Write/index.tsx
+++ src/pages/Article/Write/index.tsx
@@ -1,13 +1,14 @@
 import React, { useCallback } from 'react';
 import Vditor from 'vditor';
 import { useLocalStorageState } from 'ahooks';
-import { Layout } from 'antd';
+import { Layout, Card } from 'antd';
 import 'vditor/dist/index.css';
 
 import { article } from '@/utils/apis/article';
 
-import s from './index.module.scss';
 import ArticleLocalCache from '../articleEditCache';
+import s from './index.module.scss';
+import './index.scss';
 
 const { Content, Sider } = Layout;
 
@@ -61,7 +62,45 @@ const ArticleWrite: React.FC<Props> = ({ article }) => {
         collapsedWidth="0"
         theme="light">
         <div className={s.submitDiv}>
-          <p>这里放文章的一些设置项目</p>
+          <Card
+            title={
+              <p
+                style={{
+                  fontSize: '14px',
+                  fontWeight: 'bold',
+                  height: '100%',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'center',
+                }}>
+                发布
+              </p>
+            }
+            headStyle={{
+              padding: '1px 4px',
+              minHeight: '36px',
+            }}
+            bodyStyle={{
+              padding: '0px',
+            }}>
+            <div className={s.topPostDiv}>
+              <button className={s.savePost}>保存草稿</button>
+            </div>
+            <div className={s.postSettingDiv}>
+              <p className={s.postSettingSection}>
+                状态设置
+              </p>
+              <p className={s.postSettingSection}>
+                可见性设置
+              </p>
+              <p className={s.postSettingSection}>
+                发布时间
+              </p>
+            </div>
+            <div className={s.bottomPostDiv}>
+              <button className={s.postSubmit}>立即发布</button>
+            </div>
+          </Card>
         </div>
       </Sider>
     </Layout>
