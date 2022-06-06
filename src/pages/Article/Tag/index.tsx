b183fd390cf3d17300243ec62fba472c765504a4
Kimiato <853447650@qq.com>
2022-06-07 01:42:14 +0800

    feat: tagslist display

---
+++ src/pages/Article/Tag/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Tag } from 'antd';
+import { useRequest } from 'ahooks';
+
+import { tag, TagApi } from '@/utils/apis/tag';
+
+const TagIndex: React.FC = () => {
+  const { data, loading } = useRequest(() => TagApi.getList());
+
+  return (
+    <div>
+      <h1>tags</h1>
+      {data?.map((item: tag) => (
+        <Tag key={item.id}>{item.name}</Tag>
+      ))}
+    </div>
+  );
+}
+
+export default TagIndex;
