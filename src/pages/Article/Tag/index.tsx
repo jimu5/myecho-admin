b04d7e191773e73eb306e08add57b7270fb5e4f7
Kimiato <853447650@qq.com>
2022-06-09 01:06:06 +0800

    feat: 添加新增标签样式

--- src/pages/Article/Tag/index.tsx
+++ src/pages/Article/Tag/index.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
-import { Tag } from 'antd';
-import { useRequest } from 'ahooks';
+import { Tag, Input } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
+import { useRequest, useSafeState } from 'ahooks';
 
 import { tag, TagApi } from '@/utils/apis/tag';
 
 const TagIndex: React.FC = () => {
   const { data, loading } = useRequest(() => TagApi.getList());
+  const [inputVisible, setInputVisible] = useSafeState(false);
+  const [inputValue, setInputValue] = useSafeState('');
+
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleInputConfirm = () => {
+    setInputVisible(false);
+    setInputValue('');
+  };
+
+  const showInput = () => {
+    setInputVisible(true);
+  };
+
 
   return (
     <div>
@@ -13,6 +31,22 @@ const TagIndex: React.FC = () => {
       {data?.map((item: tag) => (
         <Tag key={item.id}>{item.name}</Tag>
       ))}
+      {inputVisible && (
+        <Input
+          type="text"
+          size="small"
+          className="tag-input"
+          value={inputValue}
+          onChange={handleInputChange}
+          onBlur={handleInputConfirm}
+          onPressEnter={handleInputConfirm}
+        />
+      )}
+      {!inputVisible && (
+        <Tag className="site-tag-plus" onClick={showInput}>
+          <PlusOutlined /> New Tag
+        </Tag>
+      )}
     </div>
   );
 };
