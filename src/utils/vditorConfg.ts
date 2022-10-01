6849a4de5957478d7905a94a435667e71ad314fd
Kimiato <853447650@qq.com>
2022-10-02 02:25:00 +0800

    feat: 增加上传文件的保存

--- src/utils/vditorConfg.ts
+++ src/utils/vditorConfg.ts
@@ -2,5 +2,6 @@ import { user } from "./axios";
 
 export var vditorUploadOptions = {
     url: "/mos/upload",
-    headers: {'Authorization': `token ${user.token}`}
+    headers: {'Authorization': `token ${user.token}`},
+    linkToImgUrl: "/mos/save_url_file",
 }
