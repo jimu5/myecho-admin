52e715c072d3defc1ba9dfdeb8dd2ccfa72f55f1
Kimiato <853447650@qq.com>
2022-11-09 01:15:25 +0800

    feat: 引入可编辑表格，将链接和站点设置的内容变成可编辑

--- src/pages/Link/ALL/index.tsx
+++ src/pages/Link/ALL/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { message, Popconfirm, Space, Table, Button } from "antd";
-import { ColumnsType } from "antd/lib/table";
+import { message, Popconfirm, Space, Button } from "antd";
+import { EditableProTable } from "@ant-design/pro-table";
+import type { ProColumns } from "@ant-design/pro-components";
 import { useRequest, useSafeState } from "ahooks";
 
 import { LinkAPI, Link } from "@/utils/apis/link";
@@ -9,37 +10,43 @@ import ModalCreate from "./modalCreate";
 
 const LinkALL: React.FC = () => {
 
+  const [editableKeys, setEditableKeys] = useSafeState<React.Key[]>([]);
   const [dataSource, setDataSource] = useSafeState<Link[]>();
   const [openModalCreate, setOpenModalCreate] = useSafeState(false);
 
-  const columns: ColumnsType<Link> = [
+  const columns: ProColumns<Link>[] = [
     {
       title: '链接名称',
       dataIndex: 'name',
       width: 140,
-      key: 'key'
     },
     {
       title: '链接描述',
       dataIndex: 'description',
       width: 100,
-      key: 'type'
     },
     {
       title: '链接地址',
       dataIndex: 'url',
-      key: 'value'
     },
     {
       title: '链接图像地址',
       dataIndex: 'icon_url',
-      key: 'value'
     },
     {
       title: '操作',
       key: 'actions',
-      render: (_, data) => (
+      valueType: 'option',
+      render: (text, data, _, action) => (
         <Space size={"middle"}>
+          <a
+            key="editable"
+            onClick={() => {
+              action?.startEditable?.(data.id)
+            }}
+          >
+            编辑
+          </a>
           <Popconfirm
             title="确定删除?"
             onConfirm={() => {
@@ -67,16 +74,30 @@ const LinkALL: React.FC = () => {
       <Button
         onClick={() => setOpenModalCreate(true)}
       >创建新的</Button>
-      <ModalCreate open={openModalCreate} setOpen={setOpenModalCreate} okCallBack={runAsync}/>
-      <Table
+      <ModalCreate open={openModalCreate} setOpen={setOpenModalCreate} okCallBack={runAsync} />
+      <EditableProTable
+        rowKey="id"
         columns={columns}
-        dataSource={dataSource}
+        value={dataSource}
+        onChange={setDataSource}
+        editable={{
+          type: 'single',
+          editableKeys,
+          onSave: async (rowKey, data, row) => {
+            LinkAPI.put(data.id, data).then(
+              message.success("保存成功")
+            )
+          },
+          onChange: setEditableKeys,
+          actionRender: (row, config, defaultDom) => [defaultDom.save, defaultDom.cancel]
+        }}
+        recordCreatorProps={false}
         pagination={false}
         loading={loading}
         style={{
           paddingTop: "20px"
         }}
-      ></Table>
+      ></EditableProTable>
     </div>
   );
 }
--- src/pages/Setting/index.tsx
+++ src/pages/Setting/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { message, Popconfirm, Space, Table, Button } from "antd";
-import { ColumnsType } from "antd/lib/table";
+import { message, Popconfirm, Space, Button } from "antd";
+import { EditableProTable } from "@ant-design/pro-table";
+import type { ProColumns } from "@ant-design/pro-components";
 import { useRequest, useSafeState } from "ahooks";
 
 import { SettingApi, settingModel } from "@/utils/apis/setting";
@@ -9,32 +10,40 @@ import ModalCreate from "./modalCreate";
 
 const Setting: React.FC = () => {
 
+  const [editableKeys, setEditableKeys] = useSafeState<React.Key[]>([]);
   const [dataSource, setDataSource] = useSafeState<settingModel[]>();
   const [openModalCreate, setOpenModalCreate] = useSafeState(false);
 
-  const columns: ColumnsType<settingModel> = [
+  const columns: ProColumns<settingModel>[] = [
     {
       title: '设置 key',
       dataIndex: 'key',
       width: 140,
-      key: 'key'
     },
     {
       title: '设置 type',
       dataIndex: 'type',
       width: 100,
-      key: 'type'
+      readonly: true
     },
     {
       title: '设置值',
       dataIndex: 'value',
-      key: 'value'
     },
     {
       title: '操作',
       key: 'actions',
-      render: (_, data) => (
+      valueType: 'option',
+      render: (text, data, _, action) => (
         <Space size={"middle"}>
+          <a
+            key="editable"
+            onClick={() => {
+              action?.startEditable?.(data.id)
+            }}
+          >
+            编辑
+          </a>
           <Popconfirm
             title="确定删除?"
             onConfirm={() => {
@@ -61,16 +70,30 @@ const Setting: React.FC = () => {
       <Button
         onClick={() => setOpenModalCreate(true)}
       >创建新的</Button>
-      <ModalCreate open={openModalCreate} setOpen={setOpenModalCreate} okCallBack={runAsync}/>
-      <Table
+      <ModalCreate open={openModalCreate} setOpen={setOpenModalCreate} okCallBack={runAsync} />
+      <EditableProTable
+        rowKey="id"
         columns={columns}
-        dataSource={dataSource}
+        value={dataSource}
+        onChange={setDataSource}
+        editable={{
+          type: 'single',
+          editableKeys,
+          onSave: async (rowKey, data, row) => {
+              SettingApi.updateValue(data.key, data.value).then(
+                message.success("保存成功")
+              )
+          },
+          onChange: setEditableKeys,
+          actionRender: (row, config, defaultDom) => [defaultDom.save, defaultDom.cancel]
+        }}
+        recordCreatorProps={false}
         pagination={false}
         loading={loading}
         style={{
           paddingTop: "20px"
         }}
-      ></Table>
+      ></EditableProTable>
     </div>
 
   );
